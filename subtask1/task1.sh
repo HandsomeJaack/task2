1f03087e3d7f219ac4c32643c0b53fa56ea56164
Dmitriy Butakov <doctor.dmitry99@gmail.com>
2021-10-08 10:51:10 +0300

    Fix formatting.

--- subtask1/task1.sh
+++ subtask1/task1.sh
@@ -3,55 +3,56 @@
 file="labelled_newscatcher_dataset.csv"
 
 usage() {
-	echo "Task 1 usage:"
-	echo "		-w | --workers I       # workers_number"
-	echo "		-s | --source_column I # source_column"
-	echo "		-d | --directory S     # folder"
-	echo "I - integer, S - string"
+    echo "Task 1 usage:"
+    echo "		-w | --workers I       # workers_number"
+    echo "		-s | --source_column I # source_column"
+    echo "		-d | --directory S     # folder"
+    echo "I - integer, S - string"
 }
 
 if ! [[ -f $file ]]; then
-	echo "No table found."
-	exit 1
+    echo "No table found."
+    exit 1
 fi
 
 while [ $# -gt 0 ]; do
-   case $1 in
-      -w|--workers)
+    case $1 in
+    -w | --workers)
         workers="$2"
         ;;
-      -s|--source_column)
+    -s | --source_column)
         source_column="$2"
         ;;
-      -d|--directory)
+    -d | --directory)
         directory="$2"
+        ;;
     esac
     shift
 done
 
-if  [[ -z $directory || -z $workers || -z $directory ]]; then
-  echo "Missing argument."
-  usage
-  exit 1
+if [[ -z $directory || -z $workers || -z $directory ]]; then
+    echo "Missing argument."
+    usage
+    exit 1
 fi
 
 # Check if workers is integer
 if ! [[ $workers =~ ^[0-9]+$ ]]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 # Check if column is integer
 if ! [[ $source_column =~ ^[0-9]+$ ]]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 # Check if column exists
 IFS=$';' read -r -a columns <$file
 if [[ $source_column > ${#columns[@]} ]]; then
-	echo "Requested column is out of range. Files contains only "${#columns[@]}" columns."
-	exit 1
+    echo "Requested column is out of range. Files contains only "${#columns[@]}" columns."
+    exit 1
 fi
 
 parallel -j $workers --skip-first-line --colsep ';' wget -P $directory -q {$source_column} :::: $file
